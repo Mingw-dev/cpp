bcdde7b8d7354c0b9969b09eb01cf31770307397
Nguyễn Minh <tuanminn120506@gmail.com>
2026-03-05 22:26:55 +0700

    nextt

--- 000.cpp
+++ 000.cpp
@@ -2,9 +2,6 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
     int t;
     cin >> t;
     while(t--) {
@@ -13,22 +10,21 @@ int main() {
 
         vector<int> a[v + 1];
         for(int i = 0 ; i < e ; i++) {
-            int u , v;
-            cin >> u >> v;
-
-            a[u].push_back(v);
-            //a[v].push_back(u);
+            int u , x;
+            cin >> u >> x;
+            a[u].push_back(x);
+            a[x].push_back(u);
         }
 
-        for(int i = 1 ; i <= v ; i++) {
+        for(int i = 1 ; i <= v; i++) {
             sort(a[i].begin() , a[i].end());
         }
 
-        for(int i = 1 ; i <= v ; i++){
+
+        for(int i = 1 ; i <= v ; i++) {
             cout << i << ": ";
-            for(auto x : a[i]) cout << x << " ";
+            for(int x : a[i]) cout << x << " ";
             cout << endl;
         }
-
     }
 }
--- 9003.cpp
+++ 9003.cpp
@@ -2,9 +2,6 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
     int t;
     cin >> t;
     while(t--) {
@@ -15,20 +12,19 @@ int main() {
         for(int i = 0 ; i < e ; i++) {
             int u , v;
             cin >> u >> v;
-
             a[u].push_back(v);
-            //a[v].push_back(u);
+            a[v].push_back(u);
         }
 
-        for(int i = 1 ; i <= v ; i++) {
+        for(int i = 1 ; i <= v; i++) {
             sort(a[i].begin() , a[i].end());
         }
 
-        for(int i = 1 ; i <= v ; i++){
+
+        for(int i = 1 ; i <= v ; i++) {
             cout << i << ": ";
-            for(auto x : a[i]) cout << x << " ";
+            for(int x : a[i]) cout << x << " ";
             cout << endl;
         }
-
     }
 }
